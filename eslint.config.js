f54f880a2a2084ac8cfa3e31181e8aacd15e91a7
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-03-11 09:43:26 -0600

    initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
 export default [
+  { ignores: ['dist'] },
   {
-    ignores: ["node_modules/", "dist/", "build/"],
-  },
-  {
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "react/react-in-jsx-scope": "off",
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-];
+]
